8094fb20e5bfa2c6b92ed69c1023fd2086d9a0e6
Daniel Dwyer <danielpatrickdwyer@gmail.com>
2016-06-30 08:16:50 -0600

    Example oAuth

---
+++ app.js
@@ -0,0 +1,127 @@
+'use strict';
+require('dotenv').config();
+
+var express = require('express')
+  , http = require('http')
+  , passport = require('passport')
+  , util = require('util')
+  , logger = require('morgan')
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
+  , methodOverride = require('method-override')
+
+  , session = require('express-session')
+  , LinkedInStrategy = require('passport-linkedin').Strategy;
+
+var app = express();
+
+// configure Express
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(logger());
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(session({ secret: 'keyboard cat' }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(__dirname + '/public'));
+
+// var LINKEDIN_API_KEY = "78e7e8fzdw8txl";
+// var LINKEDIN_SECRET_KEY = "px52IJtHucIOBqu3";
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete LinkedIn profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the LinkedInStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a token, tokenSecret, and LinkedIn profile), and
+//   invoke a callback with a user object.
+passport.use(new LinkedInStrategy({
+    consumerKey: process.env.LINKEDIN_API_KEY,
+    consumerSecret: process.env.LINKEDIN_SECRET_KEY,
+    callbackURL: "http://localhost:3000/auth/linkedin/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    console.log('user info: ', profile);
+    // asynchronous verification, for effect...
+      // To keep the example simple, the user's LinkedIn profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the LinkedIn account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+  }
+));
+
+app.get('/', function(req, res){
+  console.log(req.user);
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// GET /auth/linkedin
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in LinkedIn authentication will involve
+//   redirecting the user to linkedin.com.  After authorization, LinkedIn will
+//   redirect the user back to this application at /auth/linkedin/callback
+app.get('/auth/linkedin',
+  passport.authenticate('linkedin'),
+  function(req, res){
+    // The request will be redirected to LinkedIn for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/linkedin/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/linkedin/callback',
+  passport.authenticate('linkedin', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
